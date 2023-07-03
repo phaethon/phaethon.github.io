3d809a322b409b8d297e0128c8c68c3fc7a6158b
Eriks Dobelis <phaethon@users.noreply.github.com>
2023-07-03 22:06:22 +0300

    alt

--- frontend-mentors/interactive-rating-component/src/App.tsx
+++ frontend-mentors/interactive-rating-component/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
     <main>
       { ! submitted ?
         <div className={ styles.frame }>
-          <Circle selectable={ false }><img src="icon-star.svg" /></Circle>
+          <Circle selectable={ false }><img alt="star" src="icon-star.svg" /></Circle>
           <h1>How did we do?</h1>
           <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
           <Choice selection={ selection } setSelection={ (n: number) => setSelection(n) }/>
